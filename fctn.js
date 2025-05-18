688bb4bed463c9f092ff53d3d9a31e3910f769bd
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2025-05-18 18:21:12 +0200

    start

---
+++ fctn.js
@@ -0,0 +1,22 @@
+function toggleDetails() {
+  const x = button.nextElementSibling;
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+document.getElementById("header").innerHTML =
+
+    "<div class='container'>"
+
+    + "<div class='all'>"
+            + "<button onclick='location.href = `accueil.html`;'> Accueil </button>"
+            + "<button onclick='location.href = `projets.html`;'> Projets </button>"
+            + "<button onclick='location.href = `contacts.html` ;'> Contacts CV </button>"
+        + "</div>"
+     
+    + "</div>"
+    + "<hr class = 'hrHeader' />"
+;
